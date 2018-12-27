e63aebbcf22e8a6068789eeb148ebc90c4cc5261
devapiservice <42611322+devapiservice@users.noreply.github.com>
2018-12-27 17:15:12 -0300

    Update service.js

--- service.js
+++ service.js
@@ -1,50 +1,21 @@
-// Cargar modulos y crear nueva aplicacion
-var express = require("express"); 
-var app = express();
-var bodyParser = require('body-parser');
-app.use(bodyParser.json()); // soporte para bodies codificados en jsonsupport
-app.use(bodyParser.urlencoded({ extended: true })); // soporte para bodies codificados
- 
-//Ejemplo: GET http://localhost:8080/items
-app.get('/items', function(req, res, next) {
-  if(req.query.filter) {
-   next();
-   return;
-  }
-  res.send('Get all');
+var express = require("express"),
+    app = express(),
+    bodyParser  = require("body-parser"),
+    methodOverride = require("method-override");
+    mongoose = require('mongoose');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+   res.send("Hello World!");
 });
- 
-//Ejemplo: GET http://localhost:8080/items?filter=ABC
-app.get('/items', function(req, res) {
-  var filter = req.query.filter;
-  res.send('Get filter ' + filter);
-});
- 
-//Ejemplo: GET http://localhost:8080/items/10
-app.get('/items/:id', function(req, res, next) {
-  var itemId = req.params.id;
-  res.send('Get ' + req.params.id);
-});
- 
-//Ejemplo: POST http://localhost:8080/items
-app.post('/items', function(req, res) {
-   var data = req.body.data;
-   res.send('Add ' + data);
-});
- 
-//Ejemplo: PUT http://localhost:8080/items
-app.put('/items', function(req, res) {
-   var itemId = req.body.id;
-   var data = req.body.data;
-   res.send('Update ' + itemId + ' with ' + data);
-});
- 
-//Ejemplo: DELETE http://localhost:8080/items
-app.delete('/items/:id', function(req, res) {
-   var itemId = req.params.id;
-   res.send('Delete ' + itemId);
-});
-  
-var server = app.listen(8080, function () {
-    console.log('Server is running..'); 
+
+app.use(router);
+
+app.listen(3000, function() {
+  console.log("Node server running on http://localhost:3000");
 });
